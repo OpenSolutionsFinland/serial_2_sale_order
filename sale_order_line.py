fa859529b995e9743234e57a9383318fb44fa86a
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-11-19 09:39:03 +0200

    implementing serial to delivery order

--- sale_order_line.py
+++ sale_order_line.py
@@ -1,7 +1,7 @@
 from osv import fields, osv
-from datetime import datetime as dt
-from datetime import date
-from datetime import timedelta
+#from datetime import datetime as dt
+#from datetime import date
+#from datetime import timedelta
 
 class lot_to_sale_order_line(osv.osv_memory):
     _name='sale.order.line'
@@ -17,3 +17,25 @@ class lot_to_sale_order_line(osv.osv_memory):
     }
 
 lot_to_sale_order_line()
+
+class sales_order_with_lot(osv.osv_memory):
+    _name='sale.order.line'
+    _inherit='sale.order.line'
+    
+    def action_button_confirm(self, cr, uid, ids, context=None):
+        # Call superclass handler
+        print 'action_button_confirm'
+        # check that selected lot has correct product
+        so = self.pool.get('sale.order').browse(cr, uid, ids, context)
+        for sol in so.order_line:
+            if sol.prodlot_id:
+                if not sol.product_id.id == osl.prodlot_id.product_id.id:
+                    raise osv.except_osv('Error', 'Production lot and product do not match')
+        
+        res = super(sales_order_with_lot, self).action_button_confirm(cr, uid, ids, context)
+        
+        return res
+        
+
+sales_order_with_lot()
+        
