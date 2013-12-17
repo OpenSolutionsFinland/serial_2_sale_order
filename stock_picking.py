292fd61b8a6470ef6ea1d81e3c5d5bda7935f696
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-12-17 14:12:46 +0200

    implementing picking for desired lot

--- stock_picking.py
+++ stock_picking.py
@@ -13,6 +13,7 @@ class stock_picking_serial_from_so(osv.osv):
     '''
     
     def action_assign(self, cr, uid, ids, *args):
+        print 'action_assign'
         """ Changes state of picking to available if all moves are confirmed.
         @return: True
         """
@@ -26,8 +27,10 @@ class stock_picking_serial_from_so(osv.osv):
             
             # set prodlot for every move that has desired_prodlot_id
             for move in self.pool.get('stock.move').browse(cr, uid, move_ids):
+                print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
                 if move.desired_prodlot_id:
-                    print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
+                    #def onchange_lot_id(self, cr, uid, ids, prodlot_id=False, product_qty=False,loc_id=False, product_id=False, uom_id=False, context=None):
+                    #print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
                     self.pool.get('stock.move').write(cr, uid, [move.id], {'prodlot_id': move.desired_prodlot_id}, context=context)
                     
             self.pool.get('stock.move').action_assign(cr, uid, move_ids)
