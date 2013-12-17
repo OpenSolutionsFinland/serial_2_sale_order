5528a962cdd25e866a366a0dc5b7e452c945c7c6
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-12-17 14:27:19 +0200

    implementing picking for desired lot

--- stock_picking.py
+++ stock_picking.py
@@ -10,13 +10,22 @@ class stock_picking_serial_from_so(osv.osv):
         print str(partial_datas)
         res = super(stock_picking_serial_from_so, self).do_partial(cr, uid, ids, partial_datas, context=context)
         return res
+        # set prodlot for every move that has desired_prodlot_id
+        for move in self.pool.get('stock.move').browse(cr, uid, move_ids):
+            print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
+            if move.desired_prodlot_id:
+                #def onchange_lot_id(self, cr, uid, ids, prodlot_id=False, product_qty=False,loc_id=False, product_id=False, uom_id=False, context=None):
+                #print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
+                self.pool.get('stock.move').write(cr, uid, [move.id], {'prodlot_id': move.desired_prodlot_id}, context=context)
     '''
     
+
+    
     def action_assign(self, cr, uid, ids, *args):
-        print 'action_assign'
         """ Changes state of picking to available if all moves are confirmed.
         @return: True
         """
+        print 'action_assign'
         wf_service = netsvc.LocalService("workflow")
         for pick in self.browse(cr, uid, ids):
             if pick.state == 'draft':
@@ -24,18 +33,9 @@ class stock_picking_serial_from_so(osv.osv):
             move_ids = [x.id for x in pick.move_lines if x.state == 'confirmed']
             if not move_ids:
                 raise osv.except_osv(_('Warning!'),_('Not enough stock, unable to reserve the products.'))
-            
-            # set prodlot for every move that has desired_prodlot_id
-            for move in self.pool.get('stock.move').browse(cr, uid, move_ids):
-                print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
-                if move.desired_prodlot_id:
-                    #def onchange_lot_id(self, cr, uid, ids, prodlot_id=False, product_qty=False,loc_id=False, product_id=False, uom_id=False, context=None):
-                    #print 'move ' + str(move.id) + " has desired lot " + str(move.desired_prodlot_id)
-                    self.pool.get('stock.move').write(cr, uid, [move.id], {'prodlot_id': move.desired_prodlot_id}, context=context)
-                    
             self.pool.get('stock.move').action_assign(cr, uid, move_ids)
         return True
-    
+            
     _columns = {
         
     }
