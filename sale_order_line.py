bb8fdee513d5ec32310ebc88ba7689971d7b4400
Jesse Vartiainen <jezco@Jesses-MacBook-Pro.local>
2013-12-17 16:26:58 +0200

    classes are now subclassing osv.osv

--- sale_order_line.py
+++ sale_order_line.py
@@ -17,13 +17,14 @@ class lot_to_sale_order_line(osv.osv):
     def product_id_change(self, cr, uid, ids, pricelist, product, qty=0, uom=False, qty_uos=0, uos=False, name='', partner_id=False, lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, context=None):
         print 'product_id_change'
         print 'product ' + str(product)
+        
         res = super(lot_to_sale_order_line, self).product_id_change(cr, uid, ids, pricelist, product, qty=qty, uom=uom, qty_uos=qty_uos, uos=uos, name='', partner_id=partner_id, lang=lang, update_tax=update_tax, date_order=date_order, packaging=packaging, fiscal_position=fiscal_position, flag=flag, context=context)
         print res
         return res
     
     _columns = {
         #'dd': fields.function(_get_date, method=True, type='char',store=True, string='Delivery date'),
-        'prodlot_id': fields.many2one('stock.production.lot', 'Lot', selection=_sel_func)
+        'prodlot_id': fields.many2one('stock.production.lot', 'Lot', domain="[('prodlot_id.product_id', '=', product_id)]")
     }
     
     _defaults = {
